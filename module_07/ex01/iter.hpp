a1d8c5cedf7ee4f59fd4f0fcb3fe4735b0fa945c
RozaKerobyan <rozaqeropyan1234@gmail.com>
2026-08-30 16:05:59 +0400

    add const

--- module_07/ex01/iter.hpp
+++ module_07/ex01/iter.hpp
@@ -3,16 +3,32 @@
 #include <iostream>
 
 template <typename T>
-void iter(T *array, const size_t length, void (*f)(T&))
+void iter(T *array, const size_t length, void (*f)(const T&))
 {
     for (size_t i = 0; i < length; i++)
         f(array[i]);
 }
 
 template <typename T>
-void print(T &element)
+void iter(T *array, const size_t length, void (*f)(T&))
+{
+    for (size_t i = 0; i < length; i++)
+        f(array[i]);
+}
+
+
+template <typename T>
+void print(const T &element)
 {
     static size_t index = 0;
     std::cout << "index [" << index << "] - " << element << std::endl;
     index++;
 }
+
+/* template <typename T>
+void print(T &element)
+{
+    static size_t index = 0;
+    std::cout << "index [" << index << "] - " << element << std::endl;
+    index++;
+} */
